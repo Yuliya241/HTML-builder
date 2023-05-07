0933f976384b2858c9d2c8e2cd7f03d31326097b
Yuliya Shelenhouskaya <yulchik241@mail.ru>
2023-05-07 08:56:19 +0300

    feat: add solution to 2 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = require('process');
+const newPath = path.join(__dirname, 'text.txt');
+const writableStream = fs.createWriteStream(newPath, 'utf-8');
+
+stdout.write('Hi!Enter the text...\n');
+
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    process.exit();
+  } else {
+    writableStream.write(data);
+  }
+});
+
+process.on('SIGINT', () => {
+  process.exit();
+});
+
+process.on('exit', () => {
+  console.log('\nGoodbye!');
+});
